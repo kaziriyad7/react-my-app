5f3eeef02fc437e54227dd918526614f0f0bd031
kazi riyad <kazimohiuddinriyad77@gmail.com>
2021-02-28 01:29:02 +0600

    add just heading in my project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name : "Kazi Riyad",
+    job : "Developer"
+  }
+  var style = {
+    color: 'gray',
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit Done <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,6 +24,7 @@ function App() {
         >
           Learn React
         </a>
+        <h2 style={style}> DETELES : {person.name + " " + person.job} </h2>
       </header>
     </div>
   );
