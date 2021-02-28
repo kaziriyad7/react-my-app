59457f125d5d4d5d4b0b13d35c47bac955f4cdb6
kazi riyad <kazimohiuddinriyad77@gmail.com>
2021-02-28 22:51:07 +0600

    add a two component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
     // var style = {
     //   color: 'gray',
     // }
+    const products = [
+        {name: 'Photoshop', price: '$90.99'},
+        {name: 'Illustrator', price: '$60.99'},
+        {name: 'PDF Reader', price: '$30.99'}
+    ]
     return (
         <div className="App">
             <header className="App-header">
@@ -26,21 +31,48 @@ function App() {
         </a>
         <h2 style={style}> DETELES : {person.name + " " + person.job} </h2> */}
                 <p>I am a React Person </p>
-                <Person></Person>
+                <Product product ={products[0]}></Product>
+                <Product product ={products[1]}></Product>
+
+                <Person name = "Kazi" job = "footballer" ></Person>
+                <Person name = "Tareq" job = "Web Developer"></Person>
+                <Person name = "Adib" job = "Business Man"></Person>
+                <Person name = "Abdulla" job = "Web Developer"></Person>
             </header>
         </div>
     );
 }
-
-function Person() {
+// new component
+function Product(props) {
+    const ProductStyle = {
+        border: "1px solid gray",
+        backgroundColor : "lightgray",
+        borderRadius : '10px',
+        height : '300px',
+        width : '400px',
+        float : 'left',
+        margin : '10px'
+    };
+    const {name,price} = props.product;
+    return (
+        <div style={ProductStyle}>
+            <h1>{name}</h1>
+            <h2>{price}</h2>
+            <button>Buy Now</button>
+        </div>
+    );
+}
+function Person(props) {
     const personStyle = {
         border: "2px solid gray",
         margin: "10px",
+        backgroundColor : "green",
+        padding : "10px"
     };
     return (
         <div style={personStyle}>
-            <h1>Name : Kazi Riyad</h1>
-            <h2>Web Developer !</h2>
+            <h1>Name : {props.name}</h1>
+            <h2>Profession : {props.job}</h2>
         </div>
     );
 }
